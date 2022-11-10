1b9c07451c7507d90824ff40fcce63ebf2117b06
gliggy <leocfitz@gmail.com>
2022-11-10 08:59:59 -0500

    go down to 0, not 1

--- 1.1.2.java
+++ 1.1.2.java
@@ -3,7 +3,7 @@ public class Program
     public static void main(String[] args)
     {
         int i = 0;
-        while (i < 30) {
+        while (i <= 30) {
             System.out.println(30 - i);
             i++;
         }
