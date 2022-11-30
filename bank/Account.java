e642a9ba95b7013465f8b676962c560541716c32
gliggy <leocfitz@gmail.com>
2022-11-30 07:12:36 -0500

    tweaked bank for some reason

--- bank/Account.java
+++ bank/Account.java
@@ -1,17 +1,20 @@
 public class Account {
-	private double money;
+    private double money;
     private String username;
     private String password;
-    private static Account(double money, String username, String password) {
-        money = this.money;
+    private Account(double money, String username, String password) {
+        this.money = money;
+        this.username = username;
+        this.password = password;
     }
-  public void deposit (double amount) {
-      money += amount;
-  }
-  public double withdraw(double amount) {
-      money -= amount;
-  }
-  public double getMoney() {
-      return money;
-  }
+    public void deposit (double amount) {
+        money += amount;
+    }
+    public double withdraw(double amount) {
+        money -= amount;
+        return amount;
+    }
+    public double getMoney() {
+        return money;
+    }
 }
